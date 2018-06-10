227634efef76b012da1d71d71a14531e32365902
Jodie_Wei <40144569+jingyaoo@users.noreply.github.com>
2018-06-10 15:20:29 -0500

    Add files via upload

---
+++ p1/my-unzip.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]) {
+	if (argc <= 1) {
+		printf("my-unzip: file1 [file2 ...]\n");
+		exit(1);
+	}
+	else {
+		for (int i  = 1; i < argc; i++) {
+			FILE *fp = fopen (argv[i], "r");
+			if (fp == NULL) {
+				printf ("my-unzip: cannot open file\n");
+				exit(1);
+			}
+			char ch[512];
+			int buffer[512];
+			while (fread(buffer, sizeof(int), 1, fp) == 1) {
+			    	int count = *buffer;
+				fread(ch, sizeof(char), 1, fp);
+				for (int j = 0; j < count; j++) {
+					printf("%c", *ch);
+				}
+			}
+		}
+	}
+	return 0;
+}
